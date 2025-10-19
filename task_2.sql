45fba9e803e74c7c66a00d8aa4290850c5bd0451
Fikerte Yimer <fikerteyimer06gmail.com>
2025-10-19 15:15:53 +0300

    update

--- task_2.sql
+++ task_2.sql
@@ -45,6 +45,6 @@ CREATE TABLE IF NOT EXISTS ORDER_DETAILS (
     order_id INT NOT NULL,
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES ORDERS(order_id),
-    FOREIGN KEY (book_id) REFERENCES BOOKS(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
