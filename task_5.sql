b1be4a1f46f5230cb98a1885ebfd311eb55df55f
Fikerte Yimer <fikerteyimer06gmail.com>
2025-10-19 16:53:46 +0300

    inserting data to alx_book_store

--- task_5.sql
+++ task_5.sql
@@ -2,5 +2,5 @@
 -- This script inserts a single row into the CUSTOMER table
 -- in the database alx_book_store
 
-INSERT INTO alx_book_store.customer (customer_id, customer_name, email, address)
+INSERT INTO customer(customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
